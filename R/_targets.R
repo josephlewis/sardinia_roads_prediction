e0332ca0bc70bf99cb44cd0caa51416acc05360f
Joseph Lewis <josephlewis1992@gmail.com>
2026-03-11 15:01:45 +0000

    update targets

--- R/_targets.R
+++ R/_targets.R
@@ -18,37 +18,37 @@ list(
   }, format = "file"),
   
   tar_target(run_005, {
-    run_004
+    run_005
     source("./R/005_model_plot_rw2.R")
     "./R/005_model_plot_rw2.R"
   }, format = "file"),
   
   tar_target(run_006, {
-    run_005 
+    run_006 
     source("./R/006_model_plot_rank_pop.R")
     "./R/006_model_plot_rank_pop.R"
   }, format = "file"),
   
   tar_target(run_007, {
-    run_006
+    run_007
     source("./R/007_model_plot_rank_re.R")
     "./R/007_model_plot_rank_re.R"
   }, format = "file"),
   
   tar_target(run_008, {
-    run_007
+    run_008
     source("./R/008_model_comparison_plot.R")
     "./R/008_model_comparison_plot.R"
   }, format = "file"),
   
   tar_target(run_009, {
-    run_007
+    run_009
     source("./R/009_model_plot_rank_pop2.R")
     "./R/009_model_plot_rank_pop2.R"
   }, format = "file"),
   
   tar_target(run_010, {
-    run_007
+    run_010
     source("./R/010_model_plots_mean_curve.R")
     "./R/010_model_plots_mean_curve.R"
   }, format = "file")
